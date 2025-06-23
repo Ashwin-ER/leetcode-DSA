94f8de76e252a4c39fe2deb6a8deb2748fe0900e
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2025-06-24 00:48:22 +0530

    Time: 2035 ms (37.58%), Space: 13.5 MB (15.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,18 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        '''nums = [2,7,11,15]
-        target = 9'''
-        num = len(nums)
-        for i in range(num):
-            for j in range(i+1,num):
-                if nums[i]+nums[j]==target:
-                    #nums[i]+nums[j]
+class Solution(object):
+    def twoSum(self, nums, target):
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                if nums[i] + nums[j] == target:
                     return [i,j]
-        return []
+        #print()
+
+
+
+
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         
