de68434a01875e6acbea1219559efc53bd6f63a3
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2025-07-06 01:09:48 +0530

    Time: 30 ms (85.34%), Space: 14.9 MB (46.74%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -2,8 +2,7 @@ class Solution (object):
     def subarraySum(self, nums, k):
         count = 0
         prefix_sum = 0
-        prefix_map = {0: 1}  # Base case: prefix sum of 0 occurs once
-        
+        prefix_map = {0: 1}  
         for num in nums:
             prefix_sum += num
             if prefix_sum - k in prefix_map:
