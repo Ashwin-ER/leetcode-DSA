7ad32e1623330e8376be6d955d5f725eaf1d1b2e
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2025-07-09 18:45:48 +0530

    Time: 0 ms (100%), Space: 13.6 MB (60.99%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,11 +1,11 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        last_non_zero = 0 
+        a = 0 
 
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[last_non_zero], nums[i] = nums[i], nums[last_non_zero]
-                last_non_zero += 1
+                nums[a], nums[i] = nums[i], nums[a]
+                a = a + 1
 
         return nums
 
