f6333c1851f871ccc644c19e76762191635e8ce2
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2025-07-13 18:42:29 +0530

    Time: 3 ms (55.94%), Space: 19.9 MB (35.44%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,11 +1,15 @@
 class Solution(object):
     def reverseString(self, s):
+        #1
         """s[:] = s[::-1]
         return s"""
+
+        #2
         left = 0
         right = len(s) -1
         while right > left:
             s[right], s[left]= s[left], s[right]
             right = right -1
             left = left +1
-        return s 
+        #3
+"""        return s.reverse()"""
