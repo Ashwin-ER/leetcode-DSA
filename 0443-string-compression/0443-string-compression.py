d46eef1b1fd9af7e66b4a89eb2d36d4e6a9fa23b
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2025-07-22 23:40:35 +0530

    Time: 0 ms (100%), Space: 12.5 MB (75.89%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def compress(self, chars):
+        read = write = 0
+        n = len(chars)
+        
+        while read < n:
+            current = chars[read]
+            count = 0
+            while read < n and chars[read] == current:
+                read += 1
+                count += 1
+            
+            chars[write] = current
+            write += 1
+            
+            if count > 1:
+                for digit in str(count):
+                    chars[write] = digit
+                    write += 1
+        
+        return write
