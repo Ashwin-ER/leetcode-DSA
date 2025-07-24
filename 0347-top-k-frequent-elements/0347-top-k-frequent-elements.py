9a5b65121e6e5f57768811fe9bbddc83e1fd6207
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2025-07-25 00:22:09 +0530

    Time: 4 ms (94.67%), Space: 14.3 MB (58.1%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,5 +1,26 @@
 class Solution(object):
     def topKFrequent(self, nums, k):
-        counter = Counter(nums)
-        return [num for num, _ in counter.most_common(k)]
+        hash = {}
+
+        for i in nums:
+            if i not in hash: 
+                hash[i] = 1
+            else:
+                hash[i] += 1
+        
+        inverted = {}
+        for key, value in hash.items():
+            if value not in inverted:
+                inverted[value] = [key]
+            else:
+                inverted[value].append(key)
+        res = []
+        for value in sorted(inverted.keys(), reverse = True):
+            for num in inverted[value]:
+                res.append(num)
+                if len(res)==k:
+                    return res
+        return res
+
+    
         
