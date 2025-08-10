53b077f8dfc1ce9c44d8eec2eacc990902672d09
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2025-08-10 23:31:56 +0530

    Time: 11 ms (94.93%), Space: 12.6 MB (68.55%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,25 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        if sorted(s) == sorted(t):
-            return True 
-        return False
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+
+        d1 = {}
+
+        for i in s:
+            if i in d1:
+                d1[i] += 1
+            else:
+                d1[i] = 1
+
+        for i in t:
+            if i in d1 and d1[i] > 0:
+                d1[i] -= 1
+            else:
+                return False
+
+        return True
