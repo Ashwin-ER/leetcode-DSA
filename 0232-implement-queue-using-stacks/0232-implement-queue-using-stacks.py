5f2545aa4aaa403571d6876010cd71f2429b51b3
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2025-08-20 22:29:59 +0530

    Time: 0 ms (100%), Space: 18.1 MB (20.1%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,20 +1,22 @@
-class MyQueue(object):
+class MyQueue:
     def __init__(self):
-        self.in_stk = []
-        self.out_stk = []
-	# Push element x to the back of queue...
-    def push(self, x):
-        self.in_stk.append(x)
-	# Remove the element from the front of the queue and returns it...
-    def pop(self):
-        self.peek()
-        return self.out_stk.pop()
-	# Get the front element...
-    def peek(self):
-        if not self.out_stk:
-            while self.in_stk:
-                self.out_stk.append(self.in_stk.pop())
-        return self.out_stk[-1]
-	# Return whether the queue is empty...
-    def empty(self):
-        return not self.in_stk and not self.out_stk
+        self.stack1 = []  # For pushing new elements
+        self.stack2 = []  # For popping from the front
+
+    def push(self, x: int) -> None:
+        self.stack1.append(x) 
+
+    def pop(self) -> int:
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop() 
+
+    def peek(self) -> int:
+        if not self.stack2: 
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1] 
+
+    def empty(self) -> bool:
+        return not self.stack1 and not self.stack2 
