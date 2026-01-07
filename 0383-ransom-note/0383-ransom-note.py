85aa5feeeb10abfaaec1a5d996f8fd39f9205aa3
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2026-01-07 09:49:42 +0530

    Time: 22 ms (85.82%), Space: 13.1 MB (5.8%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,39 +1,26 @@
-from collections import defaultdict
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
-        d = defaultdict(int)
-        for char in magazine:
-            d[char] += 1
-        for char in ransomNote:
-            if char not in d or d[char] <=0:
+        magazine = list(magazine)
+        for i in range(len(ransomNote)):
+            if ransomNote[i] not in magazine:
                 return False
-            d[char] -=1
+            else:
+                magazine.remove(ransomNote[i])
         return True
-         
-        
 
 
-
-
-        """count_ransom = {}
-        count_mag = {}
-
-        for i in ransomNote:
-            if i in count_ransom:
-                count_ransom[i] +=1
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        """arr = []
+        for i in range(len(ransomNote)):
+            if i not in list(magazine):
+                return False
             else:
-                count_ransom[i] =1
-
-        for j in magazine:
-            if j in count_mag:
-                count_mag[i] +=1
-            else:
-                count_mag[i] =1
-        
-        for char in count_ransom:
-            if count_ransom[char] > count_mag.get(char, 0):
-                return False  
-
-        return False
-"""
-            
+                arr.append(i)
+                ransomNote[i].pop()
+                magazine[i].pop()
+                if len(ransomNote)==0:
+                    return True"""
