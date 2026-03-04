9f0033fb17a9635746e693d3676d12b05c41013a
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2026-03-04 13:34:55 +0530

    Create 572-SubtreeOfAnotherTree

---
+++ 572-SubtreeOfAnotherTree/572-SubtreeOfAnotherTree.py
@@ -0,0 +1,22 @@
+# Last updated: 04/03/2026, 13:34:54
+class Solution(object):
+    def isSubtree(self, root, subRoot):
+        """
+        :type root: TreeNode
+        :type subRoot: TreeNode
+        :rtype: bool
+        """ 
+        if root is None and subRoot is None:
+            return True
+        if subRoot is None:
+            return True
+        if root is None and subRoot is not None:
+            return False
+        return self.isSame(root,subRoot) or self.isSubtree(root.left,subRoot) or self.isSubtree(root.right,subRoot)
+
+    def isSame(self,root,subRoot):
+        if root is None and subRoot is None:
+            return True
+        if root is None or subRoot is None:
+            return False
+        return root.val == subRoot.val and self.isSame(root.left,subRoot.left) and self.isSame(root.right,subRoot.right)
