a4be0825a830725fab1fbf01abf2117fef5f5bfc
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2026-03-04 13:34:42 +0530

    Create 969-NumberOfRecentCalls

---
+++ 969-NumberOfRecentCalls/969-NumberOfRecentCalls.py
@@ -0,0 +1,13 @@
+# Last updated: 04/03/2026, 13:34:42
+class RecentCounter(object):
+
+    def __init__(self):
+        self.recent_counter = collections.deque()
+        
+
+    def ping(self, t):
+        self.recent_counter.append(t)
+        while (self.recent_counter[0] < t - 3000):
+            self.recent_counter.popleft()
+        return len(self.recent_counter)
+        
