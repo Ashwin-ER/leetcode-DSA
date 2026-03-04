8e5e8e851bd3cacbfd8981e0ac56256d88c3272a
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2026-03-04 13:36:20 +0530

    Create 26-RemoveDuplicatesFromSortedArray

---
+++ 26-RemoveDuplicatesFromSortedArray/26-RemoveDuplicatesFromSortedArray.py
@@ -0,0 +1,13 @@
+# Last updated: 04/03/2026, 13:36:20
+class Solution(object):
+    def removeDuplicates(self, nums):
+        if not nums:
+            return 0
+
+        insert_pos = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[insert_pos] = nums[i]
+                insert_pos = insert_pos + 1
+        return insert_pos
+        
