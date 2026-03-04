1250bf3aae34e848b73323e557b4fd29e57a297f
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2026-03-04 13:35:07 +0530

    Create 387-FirstUniqueCharacterInAString

---
+++ 387-FirstUniqueCharacterInAString/387-FirstUniqueCharacterInAString.py
@@ -0,0 +1,20 @@
+# Last updated: 04/03/2026, 13:35:07
+class Solution(object):
+    def firstUniqChar(self, s):
+        #count = Counter(s)
+        #for index , letter in enumerate(s):
+        #    if count[letter] == 1:
+        #        return index
+        #return -1
+
+        count = {}
+        for letter in s:
+            if letter in count:
+                count[letter] += 1
+            else:
+                count[letter] = 1
+        
+        for index , letter in enumerate(s):
+            if count[letter] == 1:
+                return index
+        return -1
