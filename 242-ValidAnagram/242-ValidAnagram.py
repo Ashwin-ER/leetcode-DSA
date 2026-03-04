f7a63a2918283d91e396722a55b9e60c53d1b2e4
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2026-03-04 13:35:24 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -0,0 +1,26 @@
+# Last updated: 04/03/2026, 13:35:23
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+
+        d1 = {}
+
+        for i in s:
+            if i in d1:
+                d1[i] += 1
+            else:
+                d1[i] = 1
+
+        for i in t:
+            if i in d1 and d1[i] > 0:
+                d1[i] -= 1
+            else:
+                return False
+
+        return True
