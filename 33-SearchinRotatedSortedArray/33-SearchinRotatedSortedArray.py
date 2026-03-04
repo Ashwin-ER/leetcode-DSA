b615ea924c3e03616aef1d2e12c5f0b19c93ec06
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2026-03-04 13:34:33 +0530

    Create 33-SearchinRotatedSortedArray

---
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.py
@@ -0,0 +1,8 @@
+# Last updated: 04/03/2026, 13:34:33
+1class Solution(object):
+2    def search(self, nums, target):
+3        for i in range(len(nums)):
+4            if nums[i] == target:
+5                return i
+6
+7        return -1
