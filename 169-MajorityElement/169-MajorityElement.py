aebf3bd27461f0b2375c34b3914d2195eb0f21ce
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2026-03-04 13:35:38 +0530

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.py
@@ -0,0 +1,22 @@
+# Last updated: 04/03/2026, 13:35:38
+class Solution(object):
+    def majorityElement(self, nums):
+        count = {}
+
+        for i in nums:
+            if i in count:
+                count[i] = count[i] + 1
+            else:
+                count[i] = 1
+        
+        c = 0
+
+        for i in nums:
+            if count[i] > c:
+                k = i
+                c = count[i]
+        return k
+            
+
+
+        
