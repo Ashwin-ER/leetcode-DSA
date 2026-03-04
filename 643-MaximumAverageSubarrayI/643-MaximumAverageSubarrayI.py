4fe32e1d935494efecf8abad784565a133ab9a4e
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2026-03-04 13:34:53 +0530

    Create 643-MaximumAverageSubarrayI

---
+++ 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.py
@@ -0,0 +1,18 @@
+# Last updated: 04/03/2026, 13:34:53
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        left = 0
+        right = k
+
+        Final = sum(nums[left:right])
+
+        CurrentSum = Final
+
+        while right < len(nums):
+            CurrentSum = CurrentSum - nums[left] + nums[right]
+            if CurrentSum > Final:
+                Final = CurrentSum
+            left+=1
+            right+=1
+
+        return Final/k
