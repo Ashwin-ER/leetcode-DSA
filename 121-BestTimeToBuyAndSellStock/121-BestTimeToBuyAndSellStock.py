af1f6d4267ae8b77706fa1660095cea2e9de1289
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2026-03-04 13:35:50 +0530

    Create 121-BestTimeToBuyAndSellStock

---
+++ 121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.py
@@ -0,0 +1,9 @@
+# Last updated: 04/03/2026, 13:35:50
+class Solution(object):
+    def maxProfit(self, prices):
+        maxProfit = 0
+        minPurchase = prices[0]
+        for i in range(1, len(prices)):		
+            maxProfit = max(maxProfit, prices[i] - minPurchase)
+            minPurchase = min(minPurchase, prices[i])
+        return maxProfit
