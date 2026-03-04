5c7faf10792e5b84ee0ff9146362f60c047dfaf2
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2026-03-04 13:35:09 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.py
@@ -0,0 +1,27 @@
+# Last updated: 04/03/2026, 13:35:08
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        magazine = list(magazine)
+        for i in range(len(ransomNote)):
+            if ransomNote[i] not in magazine:
+                return False
+            else:
+                magazine.remove(ransomNote[i])
+        return True
+
+
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        """arr = []
+        for i in range(len(ransomNote)):
+            if i not in list(magazine):
+                return False
+            else:
+                arr.append(i)
+                ransomNote[i].pop()
+                magazine[i].pop()
+                if len(ransomNote)==0:
+                    return True"""
