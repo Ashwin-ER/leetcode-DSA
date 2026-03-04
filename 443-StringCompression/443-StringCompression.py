c7b06b22dbd42f498bd4d1b9df602ad044009523
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2026-03-04 13:35:02 +0530

    Create 443-StringCompression

---
+++ 443-StringCompression/443-StringCompression.py
@@ -0,0 +1,23 @@
+# Last updated: 04/03/2026, 13:35:02
+class Solution(object):
+    def compress(self, chars):
+        read = 0
+        write = 0
+        n = len(chars)
+        
+        while read < n:
+            current = chars[read]
+            count = 0
+            while read < n and chars[read] == current:
+                read += 1
+                count += 1
+            
+            chars[write] = current
+            write += 1
+            
+            if count > 1:
+                for digit in str(count):
+                    chars[write] = digit
+                    write += 1
+        
+        return write
