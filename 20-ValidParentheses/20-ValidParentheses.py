6f1bfc49abe087566a401c2237a362d5ffc9d41a
Ashwin-ER <143249415+Ashwin-ER@users.noreply.github.com>
2026-03-04 13:36:25 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -0,0 +1,14 @@
+# Last updated: 04/03/2026, 13:36:24
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        d = {'(':')', '{':'}','[':']'}
+        
+        for i in s:
+            if i in d:
+                stack.append(i)
+            else:
+                if stack == [] or d[stack.pop()] != i:
+                    return False
+                  
+        return True if stack == [] else False
